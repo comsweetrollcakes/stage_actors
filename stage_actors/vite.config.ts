63f606bca49d500ab50e072aa57c5dc8b94d8917
hironori ito <h.ito@on-sight.biz>
2025-04-21 19:51:03 +0900

    #6 DB接続

---
+++ stage_actors/vite.config.ts
@@ -0,0 +1,25 @@
+import build from '@hono/vite-build/cloudflare-workers'
+import adapter from '@hono/vite-dev-server/cloudflare'
+import tailwindcss from '@tailwindcss/vite'
+import honox from 'honox/vite'
+import { defineConfig } from 'vite'
+
+export default defineConfig(({ mode }) => {
+  if (mode === "client") {
+    return {
+      plugins: [client()],
+    };
+  } else {
+
+    return {
+      plugins: [
+        honox({
+          devServer: { adapter },
+          client: { input: ['./app/style.css'] }
+        }),
+        tailwindcss(),
+        build()
+      ]
+    };
+  }
+});
